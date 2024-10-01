a02877fc49018d9f05b15fb9014edd63d397ff23
Adrian <aizqabril@gmail.com>
2024-10-01 11:11:59 +0200

    Detección de caras realizado y creación de archivo para el reconocimiento

--- faceDetection.py
+++ faceDetection.py
@@ -1,7 +1,22 @@
 import cv2
 import os
 
+IMG_DIR = './images/'
+
 if not os.path.exists("faces"):
      os.makedirs("faces")
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+count = 0
+for imageName in os.listdir('./images'):
+     print(imageName)
+     image = cv2.imread(IMG_DIR + imageName)
+
+     faces = faceClassif.detectMultiScale(image, 1.1, 5)
+
+     for(x, y, w, h) in faces:
+        face = image[y:y + h, x:x + w]
+        face = cv2.resize(face, (150, 150))
+        cv2.imwrite("faces/" + str(count) + ".jpg", face)
+        count += 1
