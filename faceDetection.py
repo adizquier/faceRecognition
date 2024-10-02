9884b6ecf41ced21d16e93c111e7ff0c0f55058e
Adrian <aizqabril@gmail.com>
2024-10-02 12:12:26 +0200

    Modularización edl codigo para la detección frontal de la cara y de perfiles

--- faceDetection.py
+++ faceDetection.py
@@ -1,22 +1,82 @@
 import cv2
-import os
+
 
 IMG_DIR = './images/'
 
-if not os.path.exists("faces"):
-     os.makedirs("faces")
-
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+profileClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_profileface.xml")
 
-count = 0
-for imageName in os.listdir('./images'):
-     print(imageName)
-     image = cv2.imread(IMG_DIR + imageName)
+
+def select_biggest_detection(detections):
+     '''
+     Metodo encargado de seleccionar la deteccion mas grande de todas las detecciones, es decir,
+     la que corresponde a la deteccion más cercana a la cámara.
+
+     Args:
+          detections: Lista que contiene todas las detecciones 
+     '''     
+     maxArea = 0
+     biggest = None
+
+     for(x, y, w, h) in detections:
+          if w*h > maxArea:
+               maxArea = w*h
+               biggest = (x, y, w, h)
+
+     return biggest       
+
+def detectFrontalFace(image):
+     '''
+     Metodo encargado de realizar detecciones de rostros frontales en una imagen
+
+     Args:
+          image: Imagen sobre la cual se realizará la detección
+     '''
 
      faces = faceClassif.detectMultiScale(image, 1.1, 5)
 
-     for(x, y, w, h) in faces:
-        face = image[y:y + h, x:x + w]
-        face = cv2.resize(face, (150, 150))
-        cv2.imwrite("faces/" + str(count) + ".jpg", face)
-        count += 1
+     face = select_biggest_detection(faces)
+     x, y, w, h = face
+
+     bigestFace = image[y:y + h, x:x + w]
+     bigestFace = cv2.resize(face, (150, 150))
+
+     return bigestFace
+
+def detectLeftProfile(image):
+     '''
+     Método encargado de realizar la detección de perfiles izquierdos
+
+     Args:
+          image: Imagen sobre la cual se realizará la detección
+     '''
+     profiles = profileClassif.detectMultiScale(image, 1.1, 5)
+
+     prof = select_biggest_detection(profiles)
+     x, y, w, h = prof
+
+     bigestProfile = image[y:y + h, x:x + w]
+     bigestProfile = cv2.resize(prof, (150, 150))   
+
+     return bigestProfile
+
+def detectRightProfile(image):
+     '''
+     Método encargado de realizar la detección de perfiles derechos
+
+     Args:
+          image: Imagen sobre la cual se realizará la detección
+     '''
+     image = cv2.flip(image, 1)
+     
+     profiles = profileClassif.detectMultiScale(image, 1.1, 5)
+
+     prof = select_biggest_detection(profiles)
+     x, y, w, h = prof
+
+     bigestProfile = image[y:y + h, x:x + w]
+     bigestProfile = cv2.resize(prof, (150, 150))   
+
+     return bigestProfile
+
+    
