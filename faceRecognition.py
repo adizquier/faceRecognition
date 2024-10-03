e0b267d9707654bde4d79bf98b7c72e9ef9c03d4
Adrian <aizqabril@gmail.com>
2024-10-03 09:21:16 +0200

    Adición de métodos get en player y creación de clase para el control del reconocimiento facial

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,80 +1,51 @@
-import cv2
-import numpy as np
-import os
+import faceDetection as fd
 import face_recognition
-import matplotlib.pyplot as plt
-
-FACES_DIR = './faces/'
-
-facesEncodings = []
-facesNames = []
+from player import player
 
 
-# Cargar las imágenes de las caras y sus codificaciones
-for fileName in os.listdir(FACES_DIR):
-    image = cv2.imread(os.path.join(FACES_DIR, fileName))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Cambiar a RGB
+class faceRecognition():
 
-    # La tupla indica que las coordenadas de la imagen de la cara comienzan en (0,0) y finalizan en (150, 150)
-    f_coding = face_recognition.face_encodings(image, known_face_locations=[(0, 150, 150, 0)])[0]
-    facesEncodings.append(f_coding)
-    facesNames.append(fileName.split(".")[0])
+    def __init__(self) -> None:
+        self.players: list[player] = []
 
+    def getNewEncoding(self, image):
+        '''
+        Método encargado de calcular un nuevo Encoding del rostro pasado por parámetros
 
-# Inicializa la captura de video. Modificar indice segun la camara
-cap = cv2.VideoCapture(2)
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        Args:
+            image: Imagen del rostro sobre la cual se calcularán los Encodings
+        Returns:
+            f_coding: Encodings calculados para el rostro
+        '''
+        f_coding = face_recognition.face_encodings(image, known_face_locations=[(0, 150, 150, 0)])[0]
+        
+        return f_coding
+    
+    def compareEncodings(self, image):
+        '''
+        Método encargado de calcular los Encodings sobre un rostro detectado y comprobar si se corresponde
+        a un usuario registrado o no
 
-plt.ion()  # Activa el modo interactivo de matplotlib
-fig, ax = plt.subplots()  # Crea una figura y ejes para mostrar la imagen
+        Args:
+            image: Imagen que contiene el rostro detectado
+        Returns:
+            bool: True si el rostro detectado pertenece a una persona registrada, False en caso contrario
 
-while True:
-    ret, frame = cap.read()
-    if not ret or frame is None or not np.any(frame):  # Verifica si el frame está vacío o negro
-        print("Error: No se puede capturar la imagen o el frame está vacío.")
-        break
+        '''
+        f_coding = face_recognition.face_encodings(image)
 
-    frame = cv2.flip(frame, 1)
-    orig = frame.copy()
-    faces = faceClassif.detectMultiScale(frame, 1.1, 5)
+        for player in self.players:
+            encoders = player.getEncoders()
 
-    for (x, y, w, h) in faces:
-        face = orig[y:y + h, x:x + w]
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)  # Cambiar a RGB
+            result = face_recognition.compare_faces(encoders, f_coding)
 
-        actual_face_encoding = face_recognition.face_encodings(face)
-
-        # Solo continúa si se encuentra un encoding
-        if actual_face_encoding:
-            actual_face_encoding = actual_face_encoding[0]
-            result = face_recognition.compare_faces(facesEncodings, actual_face_encoding)
-
-            if True in result:
-                index = result.index(True)
-                name = facesNames[index]
-                color = (125, 220, 0)
-            else:
-                name = "Desconocido"
-                color = (50, 50, 255)
-
-            # Dibuja un rectángulo y el nombre
-            cv2.rectangle(frame, (x, y + h), (x + w, y + h + 30), color, -1)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(frame, name, (x, y + h + 25), 2, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-    # Convierte la imagen a RGB para matplotlib
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    # Muestra la imagen usando matplotlib
-    ax.clear()  # Limpiar los ejes
-    ax.imshow(frame_rgb)
-    ax.axis('off')  # Opcional: ocultar los ejes
-    plt.pause(0.001)  # Pausa para permitir que se actualice la figura
-
-    # Rompe el bucle si se presiona la tecla 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-plt.close(fig)  # Cierra la figura de matplotlib
-cv2.destroyAllWindows()
+            if result: return True
+        
+        return False
+    
+    def addUser(self, name):
+        '''
+        Método encargado de añadir a un usuario. Se encargará de obtener el frontal de la cara, el perfil derecho e izquierdo para 
+        posteriormente almacenarlos en el diccionario de Encodings con la clave que el usuario introduzca.
+        '''
+        newUser = player(name)
--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 
 class player():
-    def __init__(self) -> None:
+    def __init__(self, name = "") -> None:
+
+        self.name = name
 
         self.frontal = None
         self.rightProfile = None
@@ -8,6 +10,15 @@ class player():
 
     def getEncoders(self) -> list:
         return (self.frontal, self.rightProfile, self.leftProfile)
+    
+    def getFrontal(self):
+        return self.frontal
+    
+    def getLeft(self):
+        return self.leftProfile
+    
+    def getRight(self):
+        return self.rightProfile
     
     def setFrontal(self, frontalEncoder):
         self.frontal = frontalEncoder
